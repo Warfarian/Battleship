f6f18f03f39e1632b06b9a49d618ce5dd90b688e
Mr_Robot <66792944+Warfarian@users.noreply.github.com>
2024-10-31 02:54:21 +0530

    Receive attacks function added, Ship class complete for now.

--- index.test.js
+++ index.test.js
@@ -1,15 +1,38 @@
-import { ship, gameBoard, players } from "./src/index.js";
+import { experiments } from "webpack";
+import { Ship, Gameboard, Players } from "./src/index.js";
 
 it('Class created', () => {
-    expect(ship).toBe(ship)
+    expect(Ship).toBe(Ship);
 })
 
 
 it('Game board created', () => {
-    expect(gameBoard).toBe(gameBoard)
+    expect(Gameboard).toBe(Gameboard);
 })
 
 
 it('Players ready', () => {
-    expect(players).toBe(players)
+    expect(Players).toBe(Players);
+})
+
+it('Hits calculated', () => {
+    let newShip = new Ship(4,1,true);
+    expect(newShip.hits()).toBe(2);
+})
+
+
+it('Length returned', () => {
+    let newShip = new Ship(4,1,true);
+    expect(newShip.shipLength()).toBe(4);
+})
+
+it('Sinking properly', () => {
+    let newShip = new Ship(2,2);
+    expect(newShip.isSunk()).toBe(true);
+})
+
+it("Attack works!",()=>{
+    let newGameBoard = new Gameboard();
+    newGameBoard.receiveAttack([0,1],[0,1]);
+    expect(newGameBoard.actualAttacks).toEqual([[0,1]]);
 })
--- src/index.js
+++ src/index.js
@@ -1,41 +1,71 @@
 import ('./styles.css')
 
-class ship{
-    constructor(length,hit,isSunk){
-        this.length = length(),
-        this.hit = hit(),
-        this.isSunk = isSunk();
+class Ship{
+    constructor(length,hit,currentlySunk){
+        this.length = length,
+        this.hit = hit,
+        this.currentlySunk = currentlySunk;
     }
 
-    length(){
-        let counter = 0;
-        
+    shipLength(){
+        return this.length;
     }
 
-    hit(){
-        
-    }
+    hits(){
+        while (this.hit < this.length || this.hit === 0){
+            this.hit += 1;
+            return this.hit;    
+        }
+        }
 
     isSunk(){
-
+        if (this.length === this.hit){
+            return true;
+        }
+        else{
+            return false;
+        }        
     }
 }
 
 
-class gameBoard {
-    player(){
-
+class Gameboard {
+    constructor(attacks, actualAttacks){
+        this.attacks = [];
+        this.actualAttacks = [];
     }
 
-    cpu(){
-
+    receiveAttack(attackCoordinates,actualCoordinates){
+        if (this.arraysEqual(attackCoordinates,actualCoordinates)){
+            if (!this.actualAttacks.some(coords => this.arraysEqual(coords,actualCoordinates))){
+                this.actualAttacks.push(attackCoordinates);
+                this.attacks.push(attackCoordinates);
+                    return this.actualAttacks;
+                }
+            }   
+        else{
+            if (!this.attacks.some(coords => this.arraysEqual(coords,attackCoordinates))){
+                this.attacks.push(attackCoordinates);
+                 return attacks;
+            }
+        }
+        return this.actualAttacks;
     }
+
+    arraysEqual(arr1,arr2){
+        return Array.isArray(arr1) && Array.isArray(arr2) && arr1.length === arr2.length && arr1.every((value,index) => value === arr2[index]);
+    }
+
+    winner(){
+        
+    }
+
+
 }
 
-
-class players{
+class Players{
     player = {};
     cpu = {};
 }
 
-export {ship,gameBoard,players};
+export {Ship,Gameboard,Players};
