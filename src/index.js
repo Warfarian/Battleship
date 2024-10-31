31bb069a059857637845853f99582c50ecde5be4
Mr_Robot <66792944+Warfarian@users.noreply.github.com>
2024-10-31 16:29:00 +0530

    created grids and basic UI

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import { Ship, Gameboard, Players } from "./modules/main.js";
 import('./styles.css');
+
+function createGrid(gridId, itemCount) {
+    const grid = document.getElementById(gridId);
+    
+    for (let i = 1; i <= itemCount; i++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('cell');
+        grid.appendChild(gridItem);
+    }
+}
+
+createGrid('playerGrid', 100);
+createGrid('cpuGrid', 100);
